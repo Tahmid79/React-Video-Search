6f9a2235c929c5d5aa6a1d80f5562f87dc70ffcb
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2020-02-20 00:34:26 +0800

    1st Commit

---
+++ src/apis/youtube.js
@@ -0,0 +1,15 @@
+import axios from 'axios'  ;
+
+const KEY = "" ;
+
+export default axios.create({
+
+    baseURL : 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part : 'snippet' ,
+        maxResults :  5  ,
+        key : KEY ,
+        type : 'video'
+    }
+
+}) ;
